5d579a680ec0ee5cd65935a3da6e66dcabf443b3
Marc Oggier <marc.oggier@gmail.com>
2016-06-10 12:38:56 -0800

    update

--- seaice/mbsPanda.py
+++ seaice/mbsPanda.py
@@ -22,14 +22,12 @@ import pandas as pd
 
 
 # ----------------------------------------------------------------------------------------------------------------------#
-# MBS variable
-# ----------------------------------------------------------------------------------------------------------------------#
-
-mbs_ice_surface = {2006:8, 2007:8, 2008:8, 2009:8, 2010:8, 2011:8, 2012:8, 2013:8, 2014:8}
-
-# ----------------------------------------------------------------------------------------------------------------------#
-def read(MBS_path, lcomment='n'):
+def read(mbs_path, lcomment='n'):
     """
+    :param mbs_path:
+    :param lcomment:
+    :return:
+
 		Calculate the volume fraction of brine in function of the temperature and salinity
 
 		Parameters
@@ -41,30 +39,27 @@ def read(MBS_path, lcomment='n'):
 		----------
 		vf_b: ndarray
 			array containing the mbs data in the following column
-			1 to 5  year month day hour minute
-			6       ice thickness
-			7       mean snow thickness (mean of sensor 1 to sensor 3)
-			8       snow thickness (sensor 1)
-			9       snow thickness (sensor 2)
-			10      snow thickness (sensor 3)
+ 			 1 to 5  year month day hour minute
+			 6       ice thickness
+			 7       snow thickness (sensor 1)
+			 8       snow thickness (sensor 2)
+			 9      snow thickness (sensor 3)
+			10      mean snow thickness (mean of sensor 1 to sensor 3)
 			11      water depth
 			12      water temperature
 			13      air temperature
 			14      HR
-			15...21 air thermistor
-			22      ice surface thermistor
-			23...end ice and water thermistor
-
+			15      number of thermistor in air
+			16      depth in meter of the first thermistor in ice
+			17...17+[16-1] air thermistor
+			17+[16]      first thermistor in ice
+			17+[16+1]... end ice and water thermistor
 	"""
 
     import csv
     import datetime as dt
 
-    MBSyear = int(MBS_path.split('/')[-1][3:5])
     ## CSV with dialect
-    fopen = open(MBS_path, 'rU')
-
-    # CSV dialect
     csv.register_dialect('MBS06', delimiter='\t', doublequote=False, quotechar='', lineterminator='\r\n', escapechar='',
                          quoting=csv.QUOTE_NONE)
     csv.register_dialect('MBS09', delimiter='\t', doublequote=True, quotechar='', lineterminator='\r\n', escapechar='',
@@ -72,9 +67,17 @@ def read(MBS_path, lcomment='n'):
     csv.register_dialect('MBS13', delimiter=',', doublequote=False, quotechar='', lineterminator='\n', escapechar='',
                          quoting=csv.QUOTE_NONE)
 
+    MBSyear = int(mbs_path.split('/')[-1].split('_')[-1][2:-4])
+    fopen = open(mbs_path, 'rU')
+    source = csv.reader(fopen)
+
+
+    tz = 'UTC'
+    nheader = 1
+    if MBSyear == 11:
+        nheader = 2
+
     if 13 <= MBSyear: # 2013, 2014
-        source = csv.reader(fopen)
-        nheader = 1
         col_date = 2 -1   # 2: year, 3: day of year, 4: time
         col_Hs = np.nan
         col_Tw = 7 - 1
@@ -85,14 +88,11 @@ def read(MBS_path, lcomment='n'):
         col_Hs3 = 12 - 1
         col_Hi = 13 - 1
         col_Hw = 14 - 1
-        col_Tice_00 = 22 - 1
+        col_th = 15 - 1  #
         pos_Tice_00 = 0  #
         n_th_air = 7
         n_th = 30
-        tz = 'UTC'
     elif 10 <= MBSyear < 13:  # for 2012, 2011, 2010
-        source = csv.reader(fopen, 'MBS09')
-        nheader = 0
         col_date = 2 - 1  # 2: year, 3: day of year, 4: time
         col_Tw = 7 - 1
         col_HR = 8 - 1
@@ -102,14 +102,12 @@ def read(MBS_path, lcomment='n'):
         col_Hs3 = 12 - 1
         col_Hi = 13 - 1
         col_Hw = 14 - 1
-        col_Tice_00 = 22 - 1
+        col_th = 15 - 1  #
         pos_Tice_00 = 0  #
         n_th_air = 7
         n_th = 29
-        tz = 'UTC'
     elif 8 <= MBSyear < 10 or MBSyear == 6:  # for 2006, 2008, 2009
-        source = csv.reader(fopen, 'MBS09')
-        nheader = 0
+        nheader = 1
         col_date = 2 - 1  # 2: year, 3: day of year, 4: time
         col_Tw = 7 - 1  # water temperature
         col_HR = 8 - 1  # Rel. Humidity
@@ -119,14 +117,12 @@ def read(MBS_path, lcomment='n'):
         col_Hs3 = 12 - 1  # snow pinger 3
         col_Hi = 13 - 1  # ice thickness
         col_Hw = 14 - 1  # water depth
-        col_Tice_00 = 19 - 1  #
+        col_th = 15 - 1  #
         pos_Tice_00 = 0  # depth in meter of first thermistor from ice surface
         n_th_air = 4
         n_th = 29
-        tz = 'AKST'
     elif 7 <= MBSyear < 8:  # for 2007
-        source = csv.reader(fopen, 'MBS06')
-        nheader = 0
+        nheader = 1
         col_date = 2 - 1  # 2: year, 3: day of year, 4: time
         col_Tw = 7 - 1
         col_HR = 8 - 1
@@ -136,14 +132,12 @@ def read(MBS_path, lcomment='n'):
         col_Hs3 = np.nan
         col_Hi = 11 - 1
         col_Hw = 12 - 1
-        col_Tice_00 = 15 - 1
+        col_th = 15 - 1  #
         pos_Tice_00 = 0.05  # depth in meter of first thermistor from ice surface
         n_th_air = 0
         n_th = 29
-        tz = 'UTC'
     elif 6 <= MBSyear < 7:  # for 2006, 2008, 2009
-        source = csv.reader(fopen, 'MBS09')
-        nheader = 0
+        nheader = 1
         col_date = 2 - 1  # 2: year, 3: day of year, 4: time
         col_Tw = 7 - 1  # water temperature
         col_HR = 8 - 1  # Rel. Humidity
@@ -153,11 +147,10 @@ def read(MBS_path, lcomment='n'):
         col_Hs3 = np.nan  # snow pinger 3
         col_Hi = 13 - 1  # ice thickness
         col_Hw = 14 - 1  # water depth
-        col_Tice_00 = 19 - 1  #
+        col_th = 15 - 1  #
         pos_Tice_00 = 0  # depth in meter of first thermistor from ice surface
         n_th_air = 4
         n_th = 29
-        tz = 'AKST'
     else:
         if lcomment == 'y':
             print('out of range')
@@ -171,9 +164,10 @@ def read(MBS_path, lcomment='n'):
     for row in source:
         data.append([])
         for col in row:
-            if col == '-9999' or col == '-9999.0' or col == '-9999.000' or col == 'NAN' or col == 'nan' or col == '':
-                col = np.nan
-            data[rownum].append(col)
+            if col not in ['-9999', '-9999.0', '-9999.000', 'NAN', 'nan', '', '#VALUE!']:
+                data[rownum].append(col)
+            else:
+                data[rownum].append(np.nan)
         rownum += 1
 
     dataout = []
@@ -186,45 +180,59 @@ def read(MBS_path, lcomment='n'):
 
     for ii in range(0, len(data)):
         dataout.append([])
+
         # date and time
-        d = dt.datetime(int(float(data[ii][col_date])), 1, 1) + dt.timedelta(float(data[ii][col_date - 1]) - 1) + dt.timedelta(0, dtzone)
-        dataout[ii].append(int(d.strftime("%Y")))  # 1
-        dataout[ii].append(int(d.strftime("%m")))  # 2
-        dataout[ii].append(int(d.strftime("%d")))  # 3
-        dataout[ii].append(int(d.strftime("%H")))  # 4
-        dataout[ii].append(int(d.strftime("%M")))  # 5
+        if not np.isnan(float(data[ii][col_date])):
+            d = dt.datetime(int(float(data[ii][col_date])), 1, 1) + dt.timedelta(float(data[ii][col_date - 1]) - 1) + dt.timedelta(0, dtzone)
+            dataout[ii].append(int(d.strftime("%Y")))  # 1
+            dataout[ii].append(int(d.strftime("%m")))  # 2
+            dataout[ii].append(int(d.strftime("%d")))  # 3
+            dataout[ii].append(int(d.strftime("%H")))  # 4
+            dataout[ii].append(int(d.strftime("%M")))  # 5
 
-        # ice
-        dataout[ii].append(float(data[ii][col_Hi]))  # 6
+            # ice thickness
+            dataout[ii].append(float(data[ii][col_Hi]))  # 6 Hi
 
-        # snow
-        if 6 <= MBSyear < 8:
-            dataout[ii].append(float(data[ii][col_Hs1]))  # 7
-            dataout[ii].append(np.nan)  # 8
-            dataout[ii].append(np.nan)  # 9
-        else:
-            dataout[ii].append(float(data[ii][col_Hs1]))  # 7
-            dataout[ii].append(float(data[ii][col_Hs2]))  # 8
-            dataout[ii].append(float(data[ii][col_Hs3]))  # 9
+            # snow thickness
+            if 6 <= MBSyear < 8:
+                dataout[ii].append(float(data[ii][col_Hs1]))  # 7 Hs1
+                dataout[ii].append(np.nan)  # 8 Hs2
+                dataout[ii].append(np.nan)  # 9 Hs3
+            else:
+                dataout[ii].append(float(data[ii][col_Hs1]))  # 7 Hs1
+                dataout[ii].append(float(data[ii][col_Hs2]))  # 8 Hs2
+                dataout[ii].append(float(data[ii][col_Hs3]))  # 9 Hs3
+            dataout[ii].append(np.nanmean(dataout[ii][-3:]))  # 10 mean(Hs)
 
-        # water depth
-        dataout[ii].append(float(data[ii][col_Hw]))  # 10
-        dataout[ii].append(float(data[ii][col_Tw]))  # 11
-        dataout[ii].append(float(data[ii][col_Tair]))  # 12
-        dataout[ii].append(float(data[ii][col_HR]))  # 13
-        dataout[ii].append(float(pos_Tice_00)) # 14
+            # water depth
+            dataout[ii].append(float(data[ii][col_Hw]))  # 11
 
-        # thermistor
-        for iiT in range(0, 7 - n_th_air):
-            dataout[ii].append(np.nan) # 15 to 21
-        for iiT in range(col_Tice_00, col_Tice_00 + n_th): # 22 to end
-            dataout[ii].append(float(data[ii][iiT]))
+            # water temperature
+            dataout[ii].append(float(data[ii][col_Tw]))  # 12 Air temperature
+
+            # air temperature
+            dataout[ii].append(float(data[ii][col_Tair]))  # 13 Water temperature
+
+            # relative humidity
+            dataout[ii].append(float(data[ii][col_HR]))  # 14 Relative humidity
+
+            # number of thermistor in air
+            dataout[ii].append(float(n_th_air))  # 15
+
+            # position first thermistor in ice
+            dataout[ii].append(float(pos_Tice_00))  # 16
+
+            # thermistor
+            for iiT in range(0, n_th):
+                if isinstance(data[ii][col_th+iiT], float):
+                    dataout[ii].append(float(data[ii][col_th+iiT]))  # 17...
+                else:
+                    dataout[ii].append(np.nan)
     return np.array(dataout)
 
 
 def import_core(mbs_data, day, location=None, ice_thickness=np.nan, comment = None):
     """
-
     :param mbs_data:
     :param day:
     :param ice_thickness:
@@ -239,8 +247,8 @@ def import_core(mbs_data, day, location=None, ice_thickness=np.nan, comment = No
 
     else:
         # import temperature
-        t_mbs = np.nanmean(mbs_data[day.year][index_day], axis=0)[15 + mbs_ice_surface[day.year] - 1:]
-        t_mbs_y = [0.1*ii for ii in range(t_mbs.__len__())]
+        t_mbs = np.nanmean(mbs_data[day.year][index_day], axis=0)[int(17 + np.unique(mbs_data[day.year][index_day][:, 15-1]))-1:]
+        t_mbs_y = np.array([np.unique(mbs_data[day.year][index_day][:, 16-1])+0.1*ii for ii in range(t_mbs.__len__())]).flatten()
 
         if np.isnan(t_mbs).all():
             return None
